5bc2a4ee0a12393be996cd800c8730db1764dce4
Arian-PC\Arian <akunovac@avans.nl>
2017-01-13 16:10:12 +0100

    All Windows Open now yes..

--- NinjaWPF/ViewModel/MainViewModel.cs
+++ NinjaWPF/ViewModel/MainViewModel.cs
@@ -22,9 +22,13 @@ namespace NinjaWPF.ViewModel
         /// </summary>
         /// 
         public RelayCommand OpenStoreWindow { get; set; }
+        public RelayCommand OpenInventoryWindow { get; set; }
+        public RelayCommand OpenNinjaWindow { get; set; }
         public MainViewModel()
         {
-            OpenStoreWindow = new RelayCommand(_openStoreWindow);  
+            OpenStoreWindow = new RelayCommand(OpenStore);
+            OpenInventoryWindow = new RelayCommand(OpenInventory);
+            OpenNinjaWindow = new RelayCommand(OpenNinja);
             ////if (IsInDesignMode)
             ////{
             ////    // Code runs in Blend --> create design time data.
@@ -35,10 +39,22 @@ namespace NinjaWPF.ViewModel
             ////}
         }
 
-        public void _openStoreWindow()
+        public void OpenStore()
         {
             View.GearStore Store = new View.GearStore();
             Store.Show();
+        }
+
+        public void OpenInventory()
+        {
+            View.Inventory Inventory = new View.Inventory();
+            Inventory.Show();
+        }
+        
+        private void OpenNinja()
+        {
+            View.Ninjas Ninja = new View.Ninjas();
+            Ninja.Show();
         }
         
     }
--- NinjaWPF/ViewModel/NinjasViewModel.cs
+++ NinjaWPF/ViewModel/NinjasViewModel.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace NinjaWPF.ViewModel
 {
-    public  class GearStoreViewModel
+    class NinjasViewModel
     {
-        
     }
 }
